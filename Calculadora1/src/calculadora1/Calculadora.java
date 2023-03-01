002436b494cc5a2249b3c6a2985001ee8d03f35f
dg <dg@gmail.com>
2023-03-01 17:18:06 +0100

    cambio desde la consola

--- Calculadora1/src/calculadora1/Calculadora.java
+++ Calculadora1/src/calculadora1/Calculadora.java
@@ -1,15 +1,16 @@
+
 /*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
 package calculadora1;
-
+/*Hola*\
 /**
  *
  * @author redes
  */
-public class Calculadora {
+public class Calculadora {	
     private int num1;
     private int num2;
     
